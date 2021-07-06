130238764c698f7b4f18fd7537f8ff47b45c48ea
Guilherme Rossato <guilhermrossato@gmail.com>
2021-07-06 01:28:17 -0300

    Added some new block data

--- src/data/BlockData.js
+++ src/data/BlockData.js
@@ -10,5 +10,17 @@ export default {
 	"3": {
 		texture: "ground-right.png",
 		collide: true
+	},
+	"4": {
+		texture: "ground-in.png",
+		collide: true
+	},
+	"5": {
+		texture: "ground-left-left.png",
+		collide: true
+	},
+	"6": {
+		texture: "ground-right-right.png",
+		collide: true
 	}
 }
